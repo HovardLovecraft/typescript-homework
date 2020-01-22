4553487bee1990d17682eb1e368297ee4e8882f4
Ivan Kuznetsov1 <Ivan_Kuznetsov1@epam.com>
2020-01-22 18:25:16 +0200

    one more generic example added

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ class IUserModel {
 abstract class User {
     name: string;
     password_hash: string;
-    isSuperUser?: boolean;
+    isSuperUser: boolean;
     email?: string;
 
     constructor (name: string, password_hash: string, isSuperUser?: boolean, email?: string){
@@ -37,22 +37,44 @@ abstract class User {
 class admin extends User {
     adminID: number;
 
-    constructor(name: string, password_hash: string, adminID, isSuperUser?: boolean, email?: string, ){
-        super(name, password_hash),
+    constructor(name: string, password_hash: string, adminID, isSuperUser: boolean, email?: string, ){
+        super(name, password_hash, isSuperUser, email),
         this.adminID = adminID
     }
 }
 
+class simpleUser extends User {
+    userId: number;
+
+    constructor(name: string, password_hash: string, userId, isSuperUser: boolean, email?: string, ){
+        super(name, password_hash, isSuperUser, email),
+        this.userId = userId
+    }
+}
+
 let IvanAdmin = new admin ('IvanCoolAdmin', '42isTheAnswer', 42, true );
+let IvanUser = new simpleUser ('IvanUser', '42Univers', 43, false );
 
 ////////////////////Task 5 generic
 const dto: Dto <User> = {
     name: 'Ivan',
 };
 
+const arrayOfUsers: Array <User> = [IvanAdmin, IvanUser];
+
+function toFilterSuperUsers<User> (array) : User[] {
+    return array.filter((data) => {return data.isSuperUser === true})
+}
+
+
+
 const user = new IUserModel('Ivan', '42', true, '123@gmail.com');
+
+console.log(toFilterSuperUsers(arrayOfUsers))
+//console.log(arrayOfUsers);
 console.log(IvanAdmin);
-console.log(user);
-console.log(dto);
+console.log(IvanUser);
+// console.log(user);
+// console.log(dto);
 
 
